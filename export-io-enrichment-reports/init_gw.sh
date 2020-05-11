123199a56c577452a6bac7059735844e5245f723
Brian Anderson <briananderson@Brians-Mac-mini.local>
2020-05-11 15:34:48 -0700

    updated init scripts to have SCRIPT_PATH when modifying config.json

--- export-io-enrichment-reports/init_gw.sh
+++ export-io-enrichment-reports/init_gw.sh
@@ -10,7 +10,7 @@ then
       wget -O $SCRIPT_PATH/get_mx_stats.py https://raw.githubusercontent.com/imperva/mx-toolbox/master/performance-monitoring/get_gateway_stats.py
       wget -O $SCRIPT_PATH/config.json https://raw.githubusercontent.com/imperva/mx-toolbox/master/performance-monitoring/template.config.json
       new_host=$(echo "$1" | sed -e 's/\//\\\//g')
-      sed -i -e "s/http:\/\/1.2.3.4:8086\/write?db=imperva_performance_stats/${new_host}/g" config.json
+      sed -i -e "s/http:\/\/1.2.3.4:8086\/write?db=imperva_performance_stats/${new_host}/g" $SCRIPT_PATH/config.json
       { crontab -l; echo "* * * * * /usr/bin/python /var/user-data/get_gateway_stats.py"; } | crontab -
 fi
 
