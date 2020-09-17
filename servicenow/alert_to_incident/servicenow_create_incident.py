69a9040b3bc72698cc8c6b1534575a6d63526f24
Brian Anderson <briananderson@Brians-Mac-mini.local>
2020-09-17 14:35:58 -0700

    updated documentation and remove commented log statements

--- servicenow/alert_to_incident/servicenow_create_incident.py
+++ servicenow/alert_to_incident/servicenow_create_incident.py
@@ -23,7 +23,6 @@ incident["work_notes"] += "Object Name: "+alert["object-name"]+"\n"
 incident["work_notes"] += "Violated Item: "+alert["violated-item"]
 
 def run():
-    # logging.warning(json.dumps(incident))
     headers = {"Content-Type":"application/json","Accept":"application/json"}
     change_tickets_response = requests.post(CONFIG["servicenow"]["endpoint"]+"/now/table/incident", auth=(CONFIG["servicenow"]["username"], CONFIG["servicenow"]["password"]), headers=headers, data=json.dumps(incident))
     logging.warning(json.dumps(change_tickets_response.json()))    
